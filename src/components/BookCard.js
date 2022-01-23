857c840d06e3e73332e657ee205750ca80ab624d
unknown <Hbaig055@gmail.com>
2022-01-23 18:27:49 +0530

    link bug resolved

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,32 +1,33 @@
 import React from "react";
 // import photo from '../img/book.jpg'
 
-
 function BookCard(props) {
   return (
-    
-    <div className="col-lg-4 col-md-6">
-      <a className="card" href={"/details/"+props.bookid}>
-      
-      <img src={props.bookcover} className="card-img-top" alt="no img found"></img>
-      <div className="card-body" onClick={ (event) => {console.log(event.target.value)}}>
-        <h5 className="book-title">{props.bookname}</h5>
-        <p className="book-description">{props.bookdescription}</p>
+    <a className="col-lg-4 col-md-6" href={"/details/" + props.bookid}>
+      <div className="card">
+        <img
+          src={props.bookcover}
+          className="card-img-top"
+          alt="no img found"
+        ></img>
+        <div
+          className="card-body"
+          onClick={(event) => {
+            console.log(event.target.value);
+          }}
+        >
+          <h5 className="book-title">{props.bookname}</h5>
+          <p className="book-description">{props.bookdescription}</p>
+        </div>
+        <ul className="list-group ">
+          <li className="list-group-item">Author: {props.bookauthor}</li>
+          <li className="list-group-item">Price: &#8377; {props.bookprice}</li>
+          <li className="list-group-item">Pages: {props.bookpage}</li>
+          <li className="list-group-item">Book ID: {props.bookid}</li>
+        </ul>
+        <div className="card-body"></div>
       </div>
-      <ul className="list-group ">
-        <li className="list-group-item">Author: {props.bookauthor}</li>
-        <li className="list-group-item">Price: &#8377; {props.bookprice}</li>
-        <li className="list-group-item">Pages: {props.bookpage}</li>
-        <li className="list-group-item">Book ID: {props.bookid}</li>
-      </ul>
-      <div className="card-body">
-        
-        
-      </div>
-    
-        </a>
-    </div>
-    
+    </a>
   );
 }
 
--- src/components/TrendingBook.js
+++ src/components/TrendingBook.js
@@ -2,40 +2,39 @@ import React from "react";
 
 function TrendingBook(props) {
   return (
-    <div className="col-lg-6">
-<a href={'/details/'+props.bookid}>
-    <div className="trending-book">
-      <div className="row">
-        <div className="col-lg-4">
-          <img
-            className="trendignCardImg"
-            src={props.bookcover}
-            alt="bg"
-          />
-        </div>
-        <div className="col-lg-8">
+    <a className="col-lg-6" href={"/details/" + props.bookid}>
+      <div>
+        <div className="trending-book">
           <div className="row">
-            <div className="col-12">
-              <h1>{props.bookname}</h1>
+            <div className="col-lg-4">
+              <img className="trendignCardImg" src={props.bookcover} alt="bg" />
             </div>
-            <div className="col-12">
-              <p className="book-description">{props.bookdescription}</p>
-            </div>
-            <div className="col-12">
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">Author: {props.bookauthor}</li>
-                <li className="list-group-item">Price: &#8377; {props.bookprice}</li>
-                <li className="list-group-item">Pages: {props.bookpage}</li>
-                <li className="list-group-item">Book ID: {props.bookid}</li>
-              </ul>
+            <div className="col-lg-8">
+              <div className="row">
+                <div className="col-12">
+                  <h1>{props.bookname}</h1>
+                </div>
+                <div className="col-12">
+                  <p className="book-description">{props.bookdescription}</p>
+                </div>
+                <div className="col-12">
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                      Author: {props.bookauthor}
+                    </li>
+                    <li className="list-group-item">
+                      Price: &#8377; {props.bookprice}
+                    </li>
+                    <li className="list-group-item">Pages: {props.bookpage}</li>
+                    <li className="list-group-item">Book ID: {props.bookid}</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-
     </a>
-    </div>
   );
 }
 
